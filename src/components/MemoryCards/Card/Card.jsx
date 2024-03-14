15176c1fd3dcb0e1a5060880ff4bde57e4257a15
VesNa <nat.veselowa2010@yandex.ru>
2024-03-14 12:02:23 +0000

    added shuffling method for images

--- src/components/MemoryCards/Card/Card.jsx
+++ src/components/MemoryCards/Card/Card.jsx
@@ -1,16 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useFetchData from "../../../utils/API";
 
-function Card() {
-    const cardImg = useFetchData();
-    useEffect(() => {
-        console.log(cardImg)
-    }, [])
+function Card() {   
+    const [cards, setCards] = useState([]);
+    const [score, setScore] = useState(0);
+    const { photos, reset } = useFetchData(); 
+
+    const mixCards = () => {
+
+        const mixedCards = [...photos, ...photos]
+        .sort(() => Math.random() - 0.5)
+        .map((card) => ({ ...card, key: Math.random() }))
+
+        setCards(mixedCards);
+        setScore(0);
+        reset();
+    }
     
     return (
-        <div>{cardImg.map((img, index) => (
-            <img key={img.id} src={img.src.medium} alt={`Image ${index}`} />
-        ))}</div>
+        <div>
+            <button onClick={mixCards}>Start</button>
+            {cards && cards.map((img, index) => (
+            <img key={img.index} src={img.src.medium} alt={`Image ${index}`} width="250px" height="250px" objectFit="contain"/>
+        ))}
+        </div>
     )
 }
 
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,30 +1,14 @@
 import { useState, useEffect } from "react";
 
-const randomPage = Math.round(Math.random() * (20 - 1) + 1);
-console.log(randomPage)
-const API_KEY = import.meta.env.VITE_API_KEY;
-const BASE_URL =  `https://api.pexels.com/v1/search?query=nature&orientation=square&size=small&page=${randomPage}&per_page=6`;
-
 const useFetchData = () => {
     const [photos, setPhotos] = useState([]);
-    
-    // useEffect(() => {
-    //     (async () => {
-    //         try {
-    //             const response = await fetch(BASE_URL, {
-    //                 headers: {
-    //                     Authorization: API_KEY,
-    //                 },
-    //             });
-    //             const data = await response.json();
-    //             setPhotos(data.photos);
-    //         } catch (error) {
-    //             console.error('Error fetching data:', error);
-    //         }
-    //     })();
-    // }, []);
+    const [trigger, setTrigger] = useState(0);
 
     useEffect(() => {
+        const randomPage = Math.round(Math.random() * (14 - 1) + 1);
+        const API_KEY = import.meta.env.VITE_API_KEY;
+        const BASE_URL = `https://api.pexels.com/v1/search?query=funny&orientation=square&size=small&page=${randomPage}&per_page=6`;
+
         fetch(BASE_URL, {
             headers: {
                 Authorization: API_KEY,
@@ -32,11 +16,9 @@ const useFetchData = () => {
         })
         .then(res => res.json())
         .then(data => setPhotos(data.photos));
-            
-    }, [])
+    }, [trigger]); 
 
-    return photos;
+    return { photos, reset: () => setTrigger(t => t + 1) }; 
 }
 
 export default useFetchData;
-

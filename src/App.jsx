a29a832669ce987b554a1c6065f92f574059f636
VesNa <nat.veselowa2010@yandex.ru>
2024-03-14 14:53:31 +0000

    fixed memory card name

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ProSidebar from "./components/ProSidebar/ProSidebar";
 import Games from "./components/Games/Games";
-import Card from "./components/MemoryCards/MemoryCard/MemoryCard.jsx";
+import MemoryCard from "./components/MemoryCards/MemoryCard/MemoryCard.jsx";
 import Score from "./components/Scores/Score";
 import FunFacts from "./components/FunFacts/FunFacts";
 import Contact from "./components/Contact/Contact";

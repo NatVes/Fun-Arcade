d329068d1b2141be34be08bbc7b04e28faa8b8c4
Maha <maha@Mahamudas-MacBook-Pro.local>
2024-03-14 19:15:43 +0000

    Changed the file path in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ProSidebar from "./components/ProSidebar/ProSidebar";
-import Games from "./components/Games/Games";
+import Games from "./components/Pages/Games/Games";
 import MemoryCard from "./components/MemoryCards/MemoryCard/MemoryCard.jsx";
-import Score from "./components/Scores/Score";
-import FunFacts from "./components/FunFacts/FunFacts";
-import Contact from "./components/Contact/Contact";
+import Score from "./components/Pages/Scores/Score";
+import FunFacts from "./components/Pages/FunFacts/FunFacts";
+import Contact from "./components/Pages/Contact/Contact";
 
 
 function App() {

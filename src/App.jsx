37824d0b0630be42dd9a4714e9ff9af9e1aaeb24
VesNa <nat.veselowa2010@yandex.ru>
2024-03-16 21:01:47 +0000

    added Fun Facts page, Fun Facts Info page and linked it in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Score from "./components/Pages/Scores/Score.jsx";
 import FunFacts from "./components/Pages/FunFacts/FunFacts.jsx";
 import Contact from "./components/Pages/Contact/Contact.jsx";
 import PlayGame from "./components/Pages/PlayGame/PlayGame.jsx";
+import FunFactsInfo from "./components/Pages/FunFactsInfo/FunFactsInfo.jsx";
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
         <Route path="/games/:title" element={<PlayGame />} />
         <Route path="/scores" element={<Score />} />
         <Route path="/funfacts" element={<FunFacts />} />
+        <Route path="/funfacts/:title" element={<FunFactsInfo />}/>
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>

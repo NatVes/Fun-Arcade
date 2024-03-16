b8e878a37e53d64417174a6641c5c5848765cd9b
VesNa <nat.veselowa2010@yandex.ru>
2024-03-16 18:54:22 +0000

    added score section components to the Section page, added style

--- src/components/Pages/Scores/Score.jsx
+++ src/components/Pages/Scores/Score.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import ScoreTable from "../../ScoreTable/ScoreTable";
+import "./Score.css"
 
 export default function Score() {
+
+  let MCardScore = JSON.parse(localStorage.getItem("MCardScore"));
+  console.log(MCardScore)
+
   return (
-    <div>
-      <h2>Scores </h2>
+    <div className="container">
+      <h1 className="title text-center my-5">SCORES</h1>
+      <hr />
+      <div className="row justify-content-center my-5">
+        <div className="col-md-4">
+          <h2>TIC-TAC-TOE</h2>
+        </div>
+        <div className="col-md-4">
+          <h2>MEMORY GAME</h2>
+            {MCardScore.map((score, index) => (
+              <ul key={index}>
+                  <ScoreTable name={score.name} score={score.score} />
+              </ul>
+        ))}
+        </div>
+        <div className="col-md-4">
+          <h2>HANGMAN</h2>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/ScoreTable/ScoreTable.jsx
@@ -0,0 +1,12 @@
+
+function ScoreTable({ score, name }) {
+
+    return (
+        <>
+        <li><span>NAME:</span> {name}</li>
+        <li><span>SCORE:</span> {score}</li>
+        </>
+    )
+}
+
+export default ScoreTable

12044b7b058d138f402d730bd65b800427884f9e
VesNa <nat.veselowa2010@yandex.ru>
2024-03-18 21:59:41 +0000

    stable working code for netlify function

--- functions/fetch-img/fetch-img.js
+++ functions/fetch-img/fetch-img.js
@@ -1,40 +1,23 @@
-import axios from "axios";
-import { createClient } from "pexels";
 
 const handler = async (event, context) => {
+  const API_KEY = 'dR2lQuevhNw5pOQM62nxZ1QhyyNOzoU2D7Ola9cKdlbLdOShgvU6TPCN';
 
-  // const API_KEY = import.meta.env.VITE_API_KEY;
-  const API_KEY = 'dR2lQuevhNw5pOQM62nxZ1QhyyNOzoU2D7Ola9cKdlbLdOShgvU6TPCN'
-
-  const client = createClient('dR2lQuevhNw5pOQM62nxZ1QhyyNOzoU2D7Ola9cKdlbLdOShgvU6TPCN')
-
-  const randomPage = event.queryStringParameters;
-  
+  const randomPage = event.queryStringParameters.page;
   const url = `https://api.pexels.com/v1/search?query=funny&orientation=square&size=small,medium&page=${randomPage}&per_page=6`;
 
-  try {
-    const data = await fetch(url, {
+  // try {
+    const response = await fetch(url, {
       headers: {
-        Authorization: 'dR2lQuevhNw5pOQM62nxZ1QhyyNOzoU2D7Ola9cKdlbLdOShgvU6TPCN',
-      }
-    });
-
-    const response = await data.json();
+        Authorization: `${API_KEY}`,
+        Accept: 'application/json',
+      },
+    }).then(res => res.json())
 
     return {
       statusCode: 200,
-      body: JSON.stringify(response),
-    }
-  } catch (error) {
-    return {
-      statusCode: 500,
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-      },
-      body: JSON.stringify({ error: error.message }),
-    }
-  }
+      body: JSON.stringify(response)
+    };
+
 }
 
-module.exports = { handler }
+module.exports = { handler };
--- src/utils/API.js
+++ src/utils/API.js
@@ -6,10 +6,10 @@ const useFetchData = () => {
 
     useEffect(() => {
         const randomPage = Math.round(Math.random() * (10 - 1) + 1);
-        // console.log(randomPage, typeof(randomPage))
+        
         // const API_KEY = import.meta.env.VITE_API_KEY;
-        const BASE_URL = `/netlify/functions/fetch-img?page=${randomPage}`;
-        console.log(BASE_URL)
+        const BASE_URL = `/.netlify/functions/fetch-img?page=${randomPage}`;
+        
         fetch(BASE_URL)
         .then(res => res.json())
         .then(data => setPhotos(data.photos));
